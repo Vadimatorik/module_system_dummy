9006e7275d3d58563085484d031cdf609c199cb6
Asubuhi824lia <der_anastasi84@mail.ru>
2018-01-06 13:22:03 +0700

    Добавлена abort и нормальная перезагрузка МК вместо зависания.

---
+++ cpp_system_calls.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// Перечень используемых МК.
+#ifdef STM32F205xx
+#include "stm32f205xx.h"
+#endif
+
+#ifdef STM32F207xx
+#include "stm32f207xx.h"
+#endif
+
+#ifdef STM32F215xx
+#include "stm32f215xx.h"
+#endif
+
+#ifdef STM32F217xx
+#include "stm32f217xx.h"
+#endif
+
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/stat.h>
